a3e4c5ea56e9550077c4216c3a3d378756c807a0
ko2x <55065404+ko2x@users.noreply.github.com>
2020-10-09 13:43:07 +0200

    Update attscript.sh

--- Modulos/attscript.sh
+++ Modulos/attscript.sh
@@ -62,7 +62,7 @@ fun_atts () {
 
   [[ -e /tmp/att ]] && rm /tmp/att
 
-	wget -c -P /home https://github.com/ko2x/VPSEG/blob/master/Install/versao
+	wget -c -P /home https://raw.githubusercontent.com/ko2x/VPSEG/master/Install/versao
 
 	[[ -f "/home/versao" ]] && mv /home/versao /tmp/att
 
